d5b746c33e7efe8c96d5eadbb0146f31e2dbec93
juwan06 <zome0103@gmail.com>
2025-04-13 01:01:15 +0900

    초기 커밋.

---
+++ tt.cpp/LCS(2 direction DP).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    int dp[1001][1001] = { 0 };
+
+    for (int i = 1; i <= a.size(); i++) {
+        for (int j = 1; j <= b.size(); j++) {
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    cout << dp[a.size()][b.size()];
+}
---
+++ tt.cpp/ttt.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int dp[1001][1001] = { 0 };
+    string a, b;
+    cin >> a >> b;
+}
